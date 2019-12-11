968ec20157a363ea2f45d737e0c69a2d1ad8f2cc
solsworks <solwpaul1@gmail.com>
2019-12-10 22:51:54 -0500

    Jeff

--- js/script.js
+++ js/script.js
@@ -28,10 +28,30 @@ function generateLink() {
   let mode = document.getElementById("mode").value;
   if(mode) result += mode;
 
-  let rows = document.getElementById("rows").value;
-  if(rows) result += ","+rows;
-
-  // document.getElementById("maximum").min = document.getElementById("minimum").max
+  let collumns = document.getElementById("collumns").value;
+  console.log(collumns);
+  if(collumns) result += "d.rp,"+collumns;
+  if(collumns>0) {
+    document.getElementById("collumns1").style.display = "block";
+    let collumns1 = document.getElementById("collumns1").value;
+    if(collumns1) result += ","+collumns1;
+  } if(collumns1>0) {
+    document.getElementById("collumns2").style.display = "block";
+    let collumns2 = document.getElementById("collumns2").value;
+    if(collumns2) result += ","+collumns2;
+  } if(collumns2>0) {
+    document.getElementById("collumns3").style.display = "block";
+    let collumns3 = document.getElementById("collumns3").value;
+    if(collumns3) result += ","+collumns3;
+  } if(collumns3>0) {
+    document.getElementById("collumns4").style.display = "block";
+    let collumns4 = document.getElementById("collumns4").value;
+    if(collumns4) result += ","+collumns4;
+  } if(collumns4>0) {
+    document.getElementById("collumns5").style.display = "block";
+    let collumns5 = document.getElementById("collumns5").value;
+    if(collumns5) result += ","+collumns5;
+  }
 
   let minimum = document.getElementById("minimum").value;
   if(minimum) {
@@ -44,14 +64,34 @@ function generateLink() {
     result += maximum;
   }
 
-  let format = document.getElementById("format").value;
-  if(format) result += ",fmt."+format;
+  let formats = document.getElementById("formats").value;
+  if(formats) result += "."+formats;
+  let formats1 = document.getElementById("formats1").value;
+  if(formats1) result += "."+formats1;
+  let formats2 = document.getElementById("formats2").value;
+  if(formats2) result += "."+formats2;
+  let formats3 = document.getElementById("formats3").value;
+  if(formats3) result += "."+formats3;
+  let formats4 = document.getElementById("formats4").value;
+  if(formats4) result += "."+formats4;
+  let formats5 = document.getElementById("formats5").value;
+  if(formats5) result += "."+formats5;
 
   let type = document.getElementById("type").value;
   if(type) result += ",typ"+type;
 
   let sort = document.getElementById("sort").value;
   if(sort) result += ",ss."+sort;
+  let sort1 = document.getElementById("sort1").value;
+  if(sort1) result += "."+sort1;
+  let sort2 = document.getElementById("sort2").value;
+  if(sort2) result += "."+sort2;
+  let sort3 = document.getElementById("sort3").value;
+  if(sort3) result += "."+sort3;
+  let sort4 = document.getElementById("sort4").value;
+  if(sort4) result += "."+sort4;
+  let sort5 = document.getElementById("sort5").value;
+  if(sort5) result += "."+sort5;
 
   let descending = document.getElementById("descending").checked;
   console.log(descending);
